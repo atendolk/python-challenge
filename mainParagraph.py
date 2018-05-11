b4d6f75b9ca9d4dbd68f91f8ceab046959577ff5
Anish Tendolkar <anish.tendolkar@yahoo.com>
2018-05-10 22:05:50 -0500

    all files added

--- mainParagraph.py
+++ mainParagraph.py
@@ -4,6 +4,7 @@ import os
 # Set file path
 #txtpath = "C:\\Users\\Anish Tendolkar\\datasciencebootcamp\\python_challenge\\Zip\\PyParagraph\\raw_data\\paragraph_1.txt"
 txtpath = os.path.join("Zip","PyParagraph","raw_data","paragraph_1.txt")
+txtpaths = os.path.join("Zip","PyParagraph","raw_data","paragraph_2.txt")
 
 #paragraph = open(txtpath,'r')
 num_words = 0
@@ -45,7 +46,7 @@ with open(txtpath) as paragraph:
 print("Number of words: " + str(num_words))
 print("Number of sentences: " + str(sentences))
 print("Average letter count: " + str(average_chars))
-print("Average sentence lenght: "+str(average_sentence))
+print("Average sentence lenght: "+str(average_sentence) + "\n")
 
 file_output = "mainParagraph.txt"
 with open(file_output, "w") as txt_file:
@@ -63,3 +64,59 @@ with open(file_output, "w") as txt_file:
     # txt_file.write("\n")
     # txt_file.write("Greatest Decrease: ")
     # txt_file.write(ddate + " $"+str(greatest_decrease))
+num_words1 = 0
+num_sentences1 = 0
+num_chars1 = 0
+words1 = 0
+avg = 0
+lines1 = 0
+sentences1 = 0
+num_words1 = 0
+blanklines1 = 0
+
+with open(txtpaths) as paragraph:
+    #Find out word count
+    #print (paragraph.read())
+    for line in paragraph:
+        words1 = line.split()
+        num_words1 += len(words1)
+        num_chars1 += len(line)
+
+
+        lines1 += 1
+
+        if line.startswith('\n'):
+            blanklines1 += 1
+        else:
+    # assume that each sentence ends with . or ! or ?
+    # so simply count these characters
+            sentences1 += line.count('.') + line.count('!') + line.count('?')
+         #avg word lenght
+    avg_chars = num_chars1/num_words1
+
+        #avg sentence lenght
+    avg_sentence = num_words1/sentences1
+
+    # create a list of words
+    # use None to split at any whitespace regardless of length
+    # so for instance double space counts as one space
+
+
+print("Number of words: " + str(num_words1))
+print("Number of sentences: " + str(sentences1))
+print("avg letter count: " + str(avg_chars))
+print("avg sentence lenght: "+str(avg_sentence))
+
+file_output = "mainParagraph1.txt"
+with open(file_output, "w") as txt_file:
+    txt_file.write("Number of words: ")
+    txt_file.write(str(num_words1))
+    txt_file.write("\n")
+    txt_file.write("Number of Sentences: ")
+    txt_file.write(str(sentences1))
+    txt_file.write("\n")
+    txt_file.write("avg Letter Count: ")
+    txt_file.write(str(avg_chars))
+    txt_file.write("\n")
+    txt_file.write("avg Sentence Count: ")
+    txt_file.write(str(avg_sentence))
--- test.py
+++ test.py
@@ -6,72 +6,60 @@ import csv
 #IF YOU UNCOMMENT THE LINE ABOVE THE DATA BELOW WILL RUN THROUGH BOTH THE CSV FILES AND RETURN NUMBERS BASED ON BOTH THE CSV FILES.
 #txtpath = os.path.join("Zip","PyParagraph","raw_data","paragraph_1.txt")
 #filepath = os.path.join("Zip","PyBank","raw_data","budget_data_1.csv")
-filepaths = os.path.join("Zip","PyBank","raw_data","budget_data_2.csv")
-mon = 0
-TR = 0
-chan = 0
-p = 0
-gi = 0
-gd = 9999999999999999999999
-id = ""
-dd = ""
-revenue_chans = []
-with open(filepaths) as csvfile:
-    reader = csv.reader(csvfile)
-    # for row in reader:
-    #     print(row)
-    next(reader,None)
-    data = list(reader)
-    mon = len(data)
-    for row in data:
-        cur_month = int(row[1])
-        TR += int(row[1])
-        #The average chan in revenue between mon over the entire period
-        #chan = chan
-        #PR = PR
-        chan = int(row[1]) - p
-        # print(chan)
-        #date = int(row[0])
-        # Reset the value of PR to the row I completed my analysis
-        P = int(row[1])
-        # print(PR)
+txtpaths = os.path.join("Zip","PyParagraph","raw_data","paragraph_2.txt")
+num_words1 = 0
+num_sentences1 = 0
+num_chars1 = 0
+words1 = 0
+avg = 0
+lines1 = 0
+sentences1 = 0
+num_words1 = 0
+blanklines1 = 0
 
-        # #Determine the greatest increase
-        if (chan > gi):
-            gi = chan
-            # print(type(row))
-            # print(row)
-            id = row[0]
-#greatest decrease
-        if (chan < gd):
-            gd = chan
-            dd = row[0]
+with open(txtpaths) as paragraph:
+    #Find out word count
+    #print (paragraph.read())
+    for line in paragraph:
+        words1 = line.split()
+        num_words1 += len(words1)
+        num_chars1 += len(line)
 
-        # Add to the chans list
-        revenue_chans.append(int(row[1]))
 
-    # Set the Revenue average
-    revenue_avg = sum(revenue_chans) / len(revenue_chans)
+        lines1 += 1
 
-    print("Total Months: " , mon)
-    print("Total Revenue: " + "$" , TR)
-    print("Average Change: " + "$" + str(round(sum(revenue_chans) / len(revenue_chans),2)))
-    print("Greatest Increase: " + id + " $"+str(gi) +"\n")
-    print("Greatest Decrease: " + dd + " $"+str(gd) +"\n")
+        if line.startswith('\n'):
+            blanklines1 += 1
+        else:
+    # assume that each sentence ends with . or ! or ?
+    # so simply count these characters
+            sentences1 += line.count('.') + line.count('!') + line.count('?')
+         #avg word lenght
+    avg_chars = num_chars1/num_words1
 
-    output = "PyBankAnswer2.txt"
-    with open(output, "w") as txt_file:
-        txt_file.write("Total Months: ")
-        txt_file.write(str(mon))
-        txt_file.write("\n")
-        txt_file.write("Total Revenue: ")
-        txt_file.write(str(TR))
-        txt_file.write("\n")
-        txt_file.write("Average Change: ")
-        txt_file.write(str(round(sum(revenue_chans) / len(revenue_chans),2)))
-        txt_file.write("\n")
-        txt_file.write("Greatest Increase: ")
-        txt_file.write(id + " $"+str(gi))
-        txt_file.write("\n")
-        txt_file.write("Greatest Decrease: ")
-        txt_file.write(dd + " $"+str(gd))
+        #avg sentence lenght
+    avg_sentence = num_words1/sentences1
+
+    # create a list of words
+    # use None to split at any whitespace regardless of length
+    # so for instance double space counts as one space
+
+
+print("Number of words: " + str(num_words1))
+print("Number of sentences: " + str(sentences1))
+print("avg letter count: " + str(avg_chars))
+print("avg sentence lenght: "+str(avg_sentence))
+
+file_output = "mainParagraph1.txt"
+with open(file_output, "w") as txt_file:
+    txt_file.write("Number of words: ")
+    txt_file.write(str(num_words1))
+    txt_file.write("\n")
+    txt_file.write("Number of Sentences: ")
+    txt_file.write(str(sentences1))
+    txt_file.write("\n")
+    txt_file.write("avg Letter Count: ")
+    txt_file.write(str(avg_chars))
+    txt_file.write("\n")
+    txt_file.write("avg Sentence Count: ")
+    txt_file.write(str(avg_sentence))
